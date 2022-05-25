d9953caa1c8de1347a19c7470c42c72cdbee876c
justkode <sobu0715@gmail.com>
2022-05-25 23:25:42 +0900

    feat: gRPC BidirectionalStreamingService

---
+++ grpc-server/src/main/java/justkode/grpc/server/BidirectionalStreamingService.java
@@ -0,0 +1,32 @@
+package justkode.grpc.server;
+
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.justkode.examples.lib.BidirectionalGrpc;
+import org.justkode.examples.lib.Message;
+
+@Slf4j
+@GrpcService
+public class BidirectionalStreamingService extends BidirectionalGrpc.BidirectionalImplBase {
+    @Override
+    public StreamObserver<Message> getServerResponse(StreamObserver<Message> responseObserver) {
+        log.info("Server processing gRPC bidirectional streaming.");
+
+        return new StreamObserver<>() {
+            @Override
+            public void onNext(Message value) {
+                responseObserver.onNext(value);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
+    }
+}
